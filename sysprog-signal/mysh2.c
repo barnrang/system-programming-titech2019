7b6067ddb7527c0ad7847b57f6d5b2a8ad808b43
thumwanit.n.aa <thumwanit.n.aa@cs-mac34.edu.c.titech.ac.jp>
2019-05-17 14:54:09 +0900

    finish hatten (global)

---
+++ sysprog-signal/mysh2.c
@@ -0,0 +1,105 @@
+#include "mysh.h"
+#include <signal.h> /* signal */
+#include <errno.h>    /* errno */
+#include <stdbool.h>  /* bool */
+#include <string.h>   /* strcmp */
+#include <sys/wait.h> /* wait */
+#include <unistd.h>   /* fork */
+#include <stdlib.h>
+
+/* うまく表示されない場合は，下記の 1 を 0 に切り替えて下さい */
+#if 1
+#define FIRE "\xF0\x9F\x94\xA5\n"
+#else
+#define FIRE "FIRE\n"
+#endif
+
+/**
+ * 引数の2文字列が等しければ真．NULLの場合は両方ともNULLであれば真．
+ */
+bool streql(const char *lhs, const char *rhs) {
+    return (lhs == NULL && rhs == NULL) ||
+           (lhs != NULL && rhs != NULL && strcmp(lhs, rhs) == 0);
+}
+
+void async_handler(int sig){
+  int status;
+  waitpid(-1, &status, WNOHANG);
+  write(STDERR_FILENO, FIRE, 5);
+}
+
+pid_t pid;
+
+void pause_handler(int sig){
+  // int pid = getpid();
+  LOG("%d", pid);
+  kill(pid, SIGSTOP);
+}
+
+
+int invoke_node(node_t *node) {
+    LOG("Invoke: %s", inspect_node(node));
+    // pid_t pid;
+
+
+    /* & 付きで起動しているか否か */
+    if (node->async) {
+        LOG("{&} found: async execution required");
+    }
+
+    /* 子プロセスの生成 */
+    fflush(stdout);
+    pid = fork();
+    if (pid == -1) {
+        PERROR_DIE(node->argv[0]);
+    }
+
+    if (pid == 0) {
+        /* 子プロセス側 */
+        if (execvp(node->argv[0], node->argv) == -1) {
+            PERROR_DIE(node->argv[0]);
+        }
+        return 0; /* never happen */
+    } else {
+        /* 親プロセス側 */
+
+        /* 子に独立したプロセスグループを割り振る */
+        if (setpgid(pid, pid) == -1) {
+            PERROR_DIE(NULL);
+        }
+
+        /* 生成した子プロセスを待機 */
+        int status;
+        int options = WUNTRACED;
+        if (node->async){
+          signal(SIGCHLD, async_handler);
+          return pid;
+        } else {
+          // pid_t pid_kill;
+            LOG("yehehhe");
+            signal(SIGTSTP, pause_handler);
+            pid_t waited_pid = waitpid(pid, &status, options);
+            if (waited_pid == -1) {
+                if (errno == ECHILD) {
+                    /* すでに成仏していた（何もしない） */
+                } else {
+                    PERROR_DIE(NULL);
+                }
+              } else {
+                  LOG("Waited: pid=%d, status=%d, exit_status=%d", waited_pid, status,
+                      WEXITSTATUS(status));
+                  if (WIFEXITED(status)) {
+                      write(STDERR_FILENO, FIRE, 5);
+                  }
+              }
+              return pid;
+          // kill(pid, SIGTSTP);
+          }
+        }
+
+}
+
+/* A hook point to initialize shell */
+void init_shell(void) {
+    LOG("Initializing mysh2...");
+}
---
+++ sysprog-signal/signal0.c
@@ -0,0 +1,28 @@
+#include <signal.h> /* signal */
+#include <stdio.h>
+#include <unistd.h> /* write */
+
+void handler(int sig){
+  write(STDOUT_FILENO, "無理", 1);
+}
+
+void alarm_handler(int sig){
+  write(STDOUT_FILENO, "*", 1);
+}
+
+int main() {
+    /* 課題2: 端末で ^C (Ctrl-C) をタイプしても殺せない(無視する)ようにせよ．*/
+
+    /* 課題3: 5秒ごとに標準エラー出力に「*」を表示せよ．*/
+    alarm(5);
+    signal(SIGALRM, alarm_handler);
+
+    /* 標準エラー出力にドットを表示し続ける */
+    signal(SIGINT, handler);
+    for (;;) {
+        for (int j = 0; j < 500000000; j++)
+            ; /* busy loop */
+        write(STDERR_FILENO, ".", 1);
+    }
+    return 0;
+}
